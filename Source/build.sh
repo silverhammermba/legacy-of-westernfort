b7f8e5127b4912e50e247b0b3696368fae9cc5fb
Maxwell Anselm <manselm@lutron.com>
2022-11-20 13:41:45 -0500

    update build/assemblies for Mac

--- Source/build.sh
+++ Source/build.sh
@@ -1,1 +1,1 @@
-FrameworkPathOverride=/usr/lib/mono/4.7.2-api/ exec dotnet build Source.csproj /property:Configuration=Release
+FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ exec dotnet build Source.csproj /property:Configuration=Release
