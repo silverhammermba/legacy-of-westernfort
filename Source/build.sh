3b267aeee2c572ffb257a0f05c83326394c99efc
Maxwell Anselm <manselm@lutron.com>
2022-11-24 12:04:14 -0500

    fix bad DLLs in build dir

--- Source/build.sh
+++ Source/build.sh
@@ -1,1 +1,3 @@
-FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ exec dotnet build Source.csproj /property:Configuration=Release
+FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build Source.csproj /property:Configuration=Release
+# TODO: on mac, the build dumps all of the system dlls in the Assemblies dir for some reason, which confuses the game
+find ../Assemblies -name '*.dll' -not -name Source.dll -delete
